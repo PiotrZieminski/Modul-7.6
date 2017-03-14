1d093ecd186cca27e7e85c1731414065cce975fa
PiotrZieminski <piotrzieminski@outlook.com>
2017-03-14 21:33:37 +0100

    Dodane zadanie 7.6

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'Modul6Sass/Style_inne.css': 'Modul6Sass/Style_inne.sass'
+                }
+            }
+        },
+        
+        watch: {
+            scripts: {
+                files: ['sass/*.sass'],
+                tasks: ['Modul6Sass/Style_inne.css': 'Modul6Sass/Style_inne.sass'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
+
+
+    });
+    // Load the plugins tasks
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // Default task(s).
+
+    grunt.registerTask('default', ['sass', 'imagemin']);
+
+};
+
